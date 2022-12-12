84b8e1acf53a7c7c92edc983042111755a1ebd74
CoreyJordan <coreyajordan1@gmail.com>
2022-12-12 09:39:37 -0500

    Add Simulas Test, Level 16 Challenge
    A simple 3 state switching game in which an enumeration is
    used. User input cycles endlessly between the three enumerated
    states.
    
    Took me waaaay too long to work out the logic for this one.

---
+++ Simulas Test/Simulas Test/Program.cs
@@ -0,0 +1,80 @@
+﻿State currentState = State.Locked;
+bool valid;
+string action;
+const string openChest = "open";
+const string closeChest = "close";
+const string lockChest = "lock";
+const string unlockChest = "unlock";
+
+Console.ForegroundColor = ConsoleColor.White;
+while (true)
+{
+    action = GetPlayerChoice();
+    ActOnChest(action);
+}
+
+
+
+void ActOnChest(string act)
+{
+    if (currentState == State.Locked)
+    {
+        currentState = act switch
+        {
+            unlockChest => State.Closed,
+            _ => State.Locked
+        };
+    }
+    else if (currentState == State.Closed)
+    {
+        currentState = act switch
+        {
+            openChest => State.Open,
+            lockChest => State.Locked,
+            _ => State.Closed
+        };
+    }
+    else
+    {
+        currentState = act switch
+        {
+            closeChest => State.Closed,
+            _ => State.Open
+        };
+    }
+}
+
+string GetPlayerChoice()
+{
+    valid = false;
+    string? action = "";
+    while (!valid)
+    {
+        Console.Write("The chest is ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write($"{currentState.ToString().ToLower()}");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(". What do you want to do?..");
+   
+        action = Console.ReadLine()!.ToLower();
+        
+        valid = action switch
+        {
+            openChest => true,
+            closeChest => true,
+            lockChest => true,
+            unlockChest => true,
+            _ => false
+        };
+    }
+    return action;
+}
+
+
+
+enum State
+{
+    Locked,
+    Closed,
+    Open
+}
