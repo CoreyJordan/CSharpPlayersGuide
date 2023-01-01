5f4fa48ac981d9ce8f6ae89cca252aa04b6f39a0
CoreyJordan <coreyajordan1@gmail.com>
2023-01-01 07:24:34 -0500

    Modify Room Coordinates to clarify self-adjacency
    Previously, a coordinate could be adjacent to itself and a
    variable could be adjacent to another variable at the same
    coordinate.

--- Room Coordinates/Room Coordinates/Program.cs
+++ Room Coordinates/Room Coordinates/Program.cs
@@ -2,6 +2,7 @@
 Coordinate B = new(0, 0);
 Coordinate C = new(5, 5);
 Coordinate D = new(0, 1);
+Coordinate E = new(5, 5);
 
 Console.WriteLine($"A is next to B {A.IsAdjacent(B)} expected: false");
 Console.WriteLine($"A is next to C {A.IsAdjacent(C)} expected: false");
@@ -9,9 +10,14 @@ Console.WriteLine($"A is next to D {A.IsAdjacent(D)} expected: true (diagonally)
 Console.WriteLine($"B is next to C {B.IsAdjacent(C)} expected: false");
 Console.WriteLine($"B is next to D {B.IsAdjacent(D)} expected: true");
 Console.WriteLine($"C is next to D {C.IsAdjacent(D)} expected: false");
+Console.WriteLine($"A is next to A {A.IsAdjacent(A)} expected: false, self");
+Console.WriteLine($"C is next to E {C.IsAdjacent(E)} expected: false, same coord");
+
+
+
 Console.ReadLine();
 
-public struct Coordinate
+public readonly struct Coordinate
 {
     public float X { get; }
     public float Y { get;}
@@ -22,10 +28,17 @@ public struct Coordinate
         Y = y;
     }
 
+    // We could make this static and pass in both coordinates but it makes
+    // sense to me to say "Is (This) coordinate next to the (other) coordinate.
     public bool IsAdjacent(Coordinate other)
     {
         bool isAdjacent = false;
-        if ((other.X >= X - 1 && other.X <= X + 1) &&
+        // The same coordinate is not adjacent to itself.
+        if (other.X == X && other.Y == Y)
+        {
+            isAdjacent = false;
+        }
+        else if ((other.X >= X - 1 && other.X <= X + 1) &&
             (other.Y >= Y - 1 && other.Y <= Y + 1))
         {
             isAdjacent = true;
