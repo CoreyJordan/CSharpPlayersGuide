952031acf5a178fc5d4f0d3835aeff839b1ab7db
CoreyJordan <coreyajordan1@gmail.com>
2023-01-02 07:32:40 -0500

    Add Colored Items lvl 30 challenge
    First attempt at the challenge involving generics. It is functional
    and does what the challenge asks, displaying each item in their
    assigned colors, but I can't help but feel I've not quite gotten it
    right. Will look up solution and confirm.

---
+++ Colored Items/Colored Items/Program.cs
@@ -0,0 +1,35 @@
+﻿Sword sword = new();
+Colored<Sword> blueSword = new(sword, ConsoleColor.Blue);
+
+Bow bow = new();
+Colored<Bow> redBow = new(bow, ConsoleColor.Red);
+
+Axe axe = new();
+Colored<Axe> greenAxe = new(axe, ConsoleColor.Green);
+
+blueSword.Display();
+redBow.Display();
+greenAxe.Display();
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class Colored<T>
+{
+    public T Item { get; }
+    public ConsoleColor ItemColor { get; }
+
+    public Colored(T item, ConsoleColor itemColor)
+    {
+        Item = item;
+        ItemColor = itemColor;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = ItemColor;
+        Console.WriteLine(Item.ToString());
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
