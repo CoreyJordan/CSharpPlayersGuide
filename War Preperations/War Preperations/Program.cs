c7788e85e6569c1c68fe7b2032a391d4d855ecc0
CoreyJordan <coreyajordan1@gmail.com>
2023-01-02 06:38:53 -0500

    Add War Preperations lvl 29 challenge

---
+++ War Preperations/War Preperations/Program.cs
@@ -0,0 +1,32 @@
+﻿Sword basicSword = new(Material.Iron, GemStone.None, 35, 6);
+Sword steelSword = basicSword with { Material = Material.Steel };
+Sword amberSword = basicSword with { GemStone = GemStone.Amber };
+
+Console.WriteLine(basicSword);
+Console.WriteLine(steelSword);
+Console.WriteLine(amberSword);
+Console.ReadLine();
+
+public record Sword(Material Material,
+                    GemStone GemStone,
+                    float Length,
+                    float CrossGuardWidth);
+
+public enum Material
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+public enum GemStone
+{
+    None,
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone
+}
